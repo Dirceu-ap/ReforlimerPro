7fb2d7ea5d40d0c89756533f2a08c41058186726
Dirceu-ap <dirceu-ap@hotmail.com>
2023-12-20 21:35:32 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const galleryContainer = document.getElementById('gallery-container');
+
+    // Lista de URLs das imagens
+    const imageUrls = [
+        'imagem1.jpg',
+        'imagem2.jpg',
+        'imagem3.jpg',
+        // Adicione mais URLs conforme necessário
+    ];
+
+    // Adiciona as imagens dinamicamente
+    imageUrls.forEach((url, index) => {
+        const galleryItem = document.createElement('div');
+        galleryItem.classList.add('gallery-item');
+
+        const image = document.createElement('img');
+        image.src = url;
+        image.alt = `Imagem ${index + 1}`;
+
+        galleryItem.appendChild(image);
+        galleryContainer.appendChild(galleryItem);
+    });
+});
